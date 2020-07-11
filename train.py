28f6c49cf87ae57e5e10f91f7d85b918dce2be0d
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-07-11 17:27:18 +0800

    Update train.py

--- train.py
+++ train.py
@@ -94,6 +94,10 @@ def fit_ont_epoch(net,yolo_losses,epoch,epoch_size,epoch_size_val,gen,genval,Epo
 
 
 
+#----------------------------------------------------#
+#   检测精度mAP和pr曲线计算参考视频
+#   https://www.bilibili.com/video/BV1zE411u7Vw
+#----------------------------------------------------#
 if __name__ == "__main__":
     #-------------------------------#
     #   输入的shape大小
@@ -163,6 +167,14 @@ if __name__ == "__main__":
     num_val = int(len(lines)*val_split)
     num_train = len(lines) - num_val
     
+    #------------------------------------------------------#
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    #   Init_Epoch为起始世代
+    #   Freeze_Epoch为冻结训练的世代
+    #   Epoch总训练世代
+    #   提示OOM或者显存不足请调小Batch_size
+    #------------------------------------------------------#
     if True:
         lr = 1e-3
         Batch_size = 4
