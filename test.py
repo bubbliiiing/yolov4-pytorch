2e4c5b147e29cd7fc8b6bfa6b14bfc940ba97ad0
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-09-09 15:07:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-import torch
-from torchsummary import summary
-from nets.CSPdarknet import darknet53
-from nets.yolo4 import YoloBody
 #--------------------------------------------#
 #   该部分代码只用于看网络结构，并非测试代码
 #   map测试请看get_dr_txt.py、get_gt_txt.py
 #   和get_map.py
 #--------------------------------------------#
+import torch
+from torchsummary import summary
+from nets.CSPdarknet import darknet53
+from nets.yolo4 import YoloBody
+
 if __name__ == "__main__":
     # 需要使用device来指定网络在GPU还是CPU运行
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
