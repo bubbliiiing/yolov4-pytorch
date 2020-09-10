b0756ae3c6f59e80fde6a6e6adc159f9e849681f
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-09-10 14:20:06 +0800

    Update train_with_tensorboard.py

--- train_with_tensorboard.py
+++ train_with_tensorboard.py
@@ -196,7 +196,7 @@ if __name__ == "__main__":
         lr = 1e-3
         Batch_size = 4
         Init_Epoch = 0
-        Freeze_Epoch = 25
+        Freeze_Epoch = 50
         
         optimizer = optim.Adam(net.parameters(),lr,weight_decay=5e-4)
         if Cosine_lr:
@@ -232,8 +232,8 @@ if __name__ == "__main__":
     if True:
         lr = 1e-4
         Batch_size = 2
-        Freeze_Epoch = 25
-        Unfreeze_Epoch = 50
+        Freeze_Epoch = 50
+        Unfreeze_Epoch = 100
 
         optimizer = optim.Adam(net.parameters(),lr,weight_decay=5e-4)
         if Cosine_lr:
