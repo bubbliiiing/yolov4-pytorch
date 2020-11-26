941f84f0c5b18609ea136ca93a4ec293e92d0478
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-11-26 09:59:39 +0800

    Update yolo_training.py

--- nets/yolo_training.py
+++ nets/yolo_training.py
@@ -333,7 +333,7 @@ class Generator(object):
 
         # resize image
         new_ar = w/h * rand(1-jitter,1+jitter)/rand(1-jitter,1+jitter)
-        scale = rand(.5, 1.5)
+        scale = rand(.25, 2)
         if new_ar < 1:
             nh = int(scale*h)
             nw = int(nh*new_ar)
