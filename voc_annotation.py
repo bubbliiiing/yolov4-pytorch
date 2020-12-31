49081f53973f9a94fccff9ced5347d81f06a0ba6
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-12-31 09:15:27 +0800

    Add files via upload

--- voc_annotation.py
+++ voc_annotation.py
@@ -6,7 +6,7 @@ sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
 
 def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
+    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id), encoding='utf-8')
     tree=ET.parse(in_file)
     root = tree.getroot()
 
