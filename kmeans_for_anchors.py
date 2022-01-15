236e81b34c6c66446c7d3f7e01ab4a6aae47ca34
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2022-01-15 18:20:37 +0800

    Update kmeans_for_anchors.py

--- kmeans_for_anchors.py
+++ kmeans_for_anchors.py
@@ -144,8 +144,8 @@ if __name__ == '__main__':
     for j in range(anchors_num):
         plt.scatter(data[near == j][:,0], data[near == j][:,1])
         plt.scatter(cluster[j][0], cluster[j][1], marker='x', c='black')
-    plt.show()
     plt.savefig("kmeans_for_anchors.jpg")
+    plt.show()
     print('Save kmeans_for_anchors.jpg in root dir.')
 
     cluster = cluster[np.argsort(cluster[:, 0] * cluster[:, 1])]
